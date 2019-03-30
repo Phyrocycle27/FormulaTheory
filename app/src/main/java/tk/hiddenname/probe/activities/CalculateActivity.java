16a52db55cfa1085ef18351e749f88db2f515e7c
Phyrocycle27 <1191780@mail.ru>
2019-03-30 22:12:12 +0300

    added calculating

--- app/src/main/java/tk/hiddenname/probe/activities/CalculateActivity.java
+++ app/src/main/java/tk/hiddenname/probe/activities/CalculateActivity.java
@@ -6,15 +6,24 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import tk.hiddenname.probe.R;
 import tk.hiddenname.probe.objects.Formula;
 
 public class CalculateActivity extends AppCompatActivity {
+
+    private Formula formula;
+    private HashMap<String, String> values;
+    private List<View> addedViews;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,23 +31,48 @@ public class CalculateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calculate);
         ListView listView = findViewById(R.id.listview);
 
-        Formula formula = getIntent().getParcelableExtra("formula");
+        formula = getIntent().getParcelableExtra("formula");
         ArrayAdapter<String> adapter =
                 new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, formula.getFormulas());
         listView.setAdapter(adapter);
+
+        addedViews = new ArrayList<>();
+        values = new HashMap<>();
+        for (String key : formula.getComponents()) values.put(key, null);
+
         final LinearLayout linear = findViewById(R.id.linear);
         for (String component : formula.getComponents()) {
-            addField(component, linear);
+            linear.addView(addField(component));
         }
     }
 
     @SuppressLint("SetTextI18n")
-    private void addField(String str, LinearLayout linear) {
+    private View addField(String str) {
         @SuppressLint("InflateParams") final View view = getLayoutInflater().inflate(R.layout.calculate_item, null);
         TextView later = view.findViewById(R.id.component_name);
         EditText value = view.findViewById(R.id.value);
         later.setText(str + " = ");
         if (str.equals("g")) value.setText("9.81");
-        linear.addView(view);
+        addedViews.add(view);
+        return view;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Button btn = findViewById(R.id.calculate_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View unknown = new View(getApplicationContext());
+                for (int i = 0; i < addedViews.size(); i++) {
+                    String str = ((EditText) addedViews.get(i).findViewById(R.id.value)).getText().toString();
+                    if (str.equals("")) unknown = addedViews.get(i);
+                    values.put(formula.getComponentByIndex(i), str);
+                }
+                ((EditText) unknown.findViewById(R.id.value)).setText(formula.solve(values));
+                for (String key : values.keySet()) values.put(key, null);
+            }
+        });
     }
 }
--- app/src/main/java/tk/hiddenname/probe/objects/Formula.java
+++ app/src/main/java/tk/hiddenname/probe/objects/Formula.java
@@ -5,10 +5,14 @@ import android.os.Parcel;
 import android.os.Parcelable;
 import android.util.Log;
 
+import org.mariuszgromada.math.mxparser.Expression;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Formula implements Parcelable {
+
     private static final String delimeters = "[=+\\-*(/)\\s]";
     private String name, formula;
     private int numOfFormulas;
@@ -46,6 +50,10 @@ public class Formula implements Parcelable {
         return components;
     }
 
+    public String getComponentByIndex(int index) {
+        return components[index];
+    }
+
     public String getName() {
         return name;
     }
@@ -62,6 +70,40 @@ public class Formula implements Parcelable {
         return formulas;
     }
 
+    public String solve(HashMap<String, String> map) {
+        String unknown = "", targetFormula = "";
+        // Определяем неизвестный компонент
+        for (String key : map.keySet())
+            if (map.get(key).equals("")) unknown = key;
+        // Находим формулу, подходящую для нахождения значения неизвестного компонента
+        for (String formula : formulas)
+            if (formula.startsWith(unknown + " ")) targetFormula = formula;
+        // Заменяем буквы на известные нам значения
+        String[] str = targetFormula.split("[=]");
+        targetFormula = str[1].substring(1);
+        for (String component : components)
+            targetFormula = targetFormula.replace(component, map.get(component));
+
+        // *****************Передаём полученное выражение на вычисление в новый поток***********************
+        // Создаём внутренний локальный поток для вычисления
+        final String finalTargetFormula = targetFormula;
+        class SolveThread extends Thread {
+            @Override
+            public void run() {
+                super.run();
+            }
+
+            private String calculate() {
+                Expression expression = new Expression(finalTargetFormula);
+                return String.valueOf((int) (expression.calculate()));
+            }
+        }
+        // Запускаем поток
+        SolveThread st = new SolveThread();
+        st.start();
+        // Возвращаем ответ
+        return st.calculate();
+    }
 
     public static final Parcelable.Creator<Formula> CREATOR = new Parcelable.Creator<Formula>() {
         @Override
